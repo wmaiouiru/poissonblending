ea97581d8c64410d3111364838eb4c03681b0418
wmaiouiru <wmaiouiru@gmail.com>
2016-11-15 17:55:18 -0800

    modified argument to test other sets

--- poissonblending.py
+++ poissonblending.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
+import sys
 import numpy as np
 import scipy.sparse
 import PIL.Image
 import pyamg
+import rasterio
+import argparse
+def main(argv):
+    argument_sample='python '+os.path.realpath(__file__)
+    parser = argparse.ArgumentParser(description=argument_sample)
+    parser.add_argument('-src_file',dest='src_file',nargs='?',help='src_file',default=None)
+    parser.add_argument('-ref_file',dest='ref_file',nargs='?',help='ref_file',default=None)
+    parser.add_argument('-output_file',dest='output_file',nargs='?',help='output_file',default=None)
+    parser.add_argument('-mask_file',dest='mask_file',nargs='?',help='mask_file',default=None)
 
+    args = parser.parse_args()
+
+    srcpath=args.src_file
+    refpath=args.ref_file
+    dstpath=args.output_file
+    mask_file=args.mask_file
+    if srcpath and refpath and dstpath:
+        with rasterio.open(srcpath) as src:
+            r = src.read(1)
+
+            mask = np.clip(r, 0, 1)
+        img_source = np.asarray(PIL.Image.open(srcpath))
+        img_source.flags.writeable = True
+        img_target = np.asarray(PIL.Image.open(refpath))
+        img_target.flags.writeable = True
+        img_ret = blend(img_target, img_source, mask)
+        img_ret = PIL.Image.fromarray(np.uint8(img_ret))
+        img_ret.save(dstpath)
+    else:
+        print parser.print_help()
+        return 0
 # pre-process the mask array so that uint64 types from opencv.imread can be adapted
 def prepare_mask(mask):
     if type(mask[0][0]) is np.ndarray:
-        result = np.ndarray((mask.shape[0], mask.shape[1]), dtype=np.uint8)
+        result = np.ndarray((mask.shape[0], mask.shape[1]), dtype=np.uint16)
         for i in range(mask.shape[0]):
             for j in range(mask.shape[1]):
                 if sum(mask[i][j]) > 0:
@@ -38,6 +69,7 @@ def blend(img_target, img_source, img_mask, offset=(0, 0)):
     img_mask = prepare_mask(img_mask)
     img_mask[img_mask==0] = False
     img_mask[img_mask!=False] = True
+    print 'mask prepared '
 
     # create coefficient matrix
     A = scipy.sparse.identity(np.prod(region_size), format='lil')
@@ -55,7 +87,7 @@ def blend(img_target, img_source, img_mask, offset=(0, 0)):
                 if index-region_size[1] >= 0:
                     A[index, index-region_size[1]] = -1
     A = A.tocsr()
-    
+    print 'created coefficient matrix'
     # create poisson matrix for b
     P = pyamg.gallery.poisson(img_mask.shape)
 
@@ -101,4 +133,4 @@ def test():
 
 
 if __name__ == '__main__':
-    test()
+    main(sys.argv[1:])
